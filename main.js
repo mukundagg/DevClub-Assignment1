527a130eee687b26d4e893c625d0a63dc465e020
Mukund Aggarwal <le.rouge.lion@gmail.com>
2022-01-28 17:19:58 +0530

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-var listOfDisplayElems = document.getElementsByClassName("bodyDisplayElem"), distFromTop = 0;
-    for(var i = 0; i < listOfDisplayElems.length; i++) {
-        listOfDisplayElems[i].style.top = distFromTop + "px";
-        distFromTop += listOfDisplayElems[i].getBoundingClientRect().height;
-}
